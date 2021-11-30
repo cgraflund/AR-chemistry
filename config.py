9b5e4cbb7dc3c24c351e5a725b725c41bb23e5f8
Connor Graflund <cgraflund@mymail.mines.edu>
2021-11-30 16:21:04 -0700

    Elements appear over Aruco tags.

---
+++ config.py
@@ -0,0 +1,21 @@
+DEBUG = False
+
+# Aruco ID numbers
+H_ID = 76
+C_ID = 571
+N_ID = 658
+O_ID = 888
+
+# Aruco tag length
+marker_length = 1.77165 # inches
+
+# Camera Parameters
+Fx = 675
+Fy = 675
+f = 675
+x_c = 320
+y_c = 240
+
+# Element parameters
+height = 3
+radius = 25
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import math
 import cv2
 import sys
+import config
 
 def angle_to_rot(ax, ay, az):
     sx, sy, sz = np.sin(ax), np.sin(ay), np.sin(az)
@@ -21,23 +22,17 @@ def main():
         sys.exit()
 
     image_height, image_width = img.shape[:2]
-
-    # Camera params
-    Fx = 675
-    Fy = 675
-    f = 675
-    x_c = 320
-    y_c = 240
-
-    marker_length = 2 # inches
+    
     dist_coeff = np.zeros((1,4))
 
     K = np.array([
-        [f, 0, x_c],
-        [0, f, y_c],
+        [config.f, 0, config.x_c],
+        [0, config.f, config.y_c],
         [0, 0, 1]]).astype(float)
 
-    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_100)
+    points = np.array([[0, 0, 0, 1]]).T
+
+    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
 
     while True:
         got_image, img = video_capture.read()
@@ -47,40 +42,68 @@ def main():
             dictionary=arucoDict
         )
         if ids is not None:
-            img = cv2.aruco.drawDetectedMarkers(
-            image=img, corners=corners, ids=ids, borderColor=(0, 0, 255))
+            if config.DEBUG:
+                img = cv2.aruco.drawDetectedMarkers(
+                image=img, corners=corners, ids=ids, borderColor=(0, 0, 255))
 
             rvecs,tvecs,_ = cv2.aruco.estimatePoseSingleMarkers(
-                corners=corners, markerLength=marker_length,
+                corners=corners, markerLength=config.marker_length,
                 cameraMatrix=K, distCoeffs=dist_coeff
             )
 
-            # Get the pose of the first detected marker with respect to the camera.
-            rvec_m_c = rvecs[0]                 # This is a 1x3 rotation vector
-            tm_c = tvecs[0]                     # This is a 1x3 translation vector
+            i = 0
+            for id in ids:
+                # Get the pose of the detected marker with respect to the camera.
+                rvec_m_c = rvecs[i]                 # This is a 1x3 rotation vector
+                tm_c = tvecs[i]                     # This is a 1x3 translation vector
 
-            img = cv2.aruco.drawAxis(
-                image=img, cameraMatrix=K, distCoeffs=dist_coeff,
-                rvec=rvec_m_c, tvec=tm_c, length=marker_length
-            )
+                if config.DEBUG:
+                    img = cv2.aruco.drawAxis(
+                        image=img, cameraMatrix=K, distCoeffs=dist_coeff,
+                        rvec=rvec_m_c, tvec=tm_c, length=config.marker_length
+                    )
 
-            R_m_p = np.array([[1, 0, 0],
-                              [0, 1, 0],
-                              [0, 0, 1]])
+                R_m_p = np.array([[1, 0, 0],
+                                [0, 1, 0],
+                                [0, 0, 1]])
 
-            t_m_p = np.array([[0, 0, 5]]).T
+                t_m_p = np.array([[0, 0, -config.height]]).T
 
-            R_m_c = cv2.Rodrigues(rvec_m_c)[0]
+                R_m_c = cv2.Rodrigues(rvec_m_c)[0]
 
-            H_M_C = np.block([[R_m_c, tm_c.T], [0,0,0,1]])
+                H_M_C = np.block([[R_m_c, tm_c.T], [0,0,0,1]])
 
-            H_M_P= np.block([[R_m_p, t_m_p], [0,0,0,1]])
+                H_M_P= np.block([[R_m_p, t_m_p], [0,0,0,1]])
 
-            H_P_M = np.linalg.inv(H_M_P)
+                H_P_M = np.linalg.inv(H_M_P)
 
-            H_P_C =  H_M_C @ H_P_M
+                H_P_C =  H_M_C @ H_P_M
 
-            Mext = H_P_C[0:3, :]
+                Mext = H_P_C[0:3, :]
+
+                p = K @ Mext @ points
+                p = p / p[2]
+
+                p = p.T.astype(int)
+
+                print(f"x: {p[0][0]}, y: {p[0][1]}")
+
+                # Do something with mext to project image
+                text_offset = 15
+                if (id == config.H_ID):
+                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 0, 255), -1)
+                    cv2.putText(img, "H", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+                elif (id == config.C_ID):
+                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 0), -1)
+                    cv2.putText(img, "C", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+                elif (id == config.N_ID):
+                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 255), -1)
+                    cv2.putText(img, "N", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+                elif (id == config.O_ID):
+                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (255, 0, 0), -1)
+                    cv2.putText(img, "O", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+
+                i += 1
 
         cv2.imshow('Window', img)
 
