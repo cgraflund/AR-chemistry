de114bb5c97c8d839c8649009d9cc633cc1ad6ba
Connor Graflund <cgraflund@mymail.mines.edu>
2021-12-06 13:37:53 -0700

    Implemented molecules.

--- config.py
+++ config.py
@@ -1,10 +1,16 @@
+import numpy as np
+import cv2
+
 DEBUG = False
+record = False
 
 # Aruco ID numbers
 H_ID = 76
 C_ID = 571
 N_ID = 658
 O_ID = 888
+
+aruco_id_map = {H_ID: "H", C_ID: "C", N_ID: "N", O_ID: "O"}
 
 # Aruco tag length
 marker_length = 1.77165 # inches
@@ -16,6 +22,34 @@ f = 675
 x_c = 320
 y_c = 240
 
+K = np.array([
+        [f, 0, x_c],
+        [0, f, y_c],
+        [0, 0, 1]]).astype(float)
+
+dist_coeff = np.zeros((1,4))
+
+arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
+
 # Element parameters
 height = 3
 radius = 25
+mol_radius = 60
+text_offset = 15
+mol_text_offset = 45
+mol_name_offset = 80
+
+dist_threshold = 3
+
+# Molecules
+num_elements = 4
+
+molecules = {"H2O": {"H": 2, "C": 0, "N": 0, "O": 1},
+             "CO2": {"H": 0, "C": 1, "N": 0, "O": 2},
+             "N2": {"H": 0, "C": 0, "N": 2, "O": 0},
+             "O3": {"H": 0, "C": 0, "N": 0, "O": 3},
+             "NH3": {"H": 3, "C": 0, "N": 1, "O": 0},
+             "NH4": {"H": 4, "C": 0, "N": 1, "O": 0},
+             "O2": {"H": 0, "C": 0, "N": 0, "O": 2},
+             "CH4": {"H": 4, "C": 1, "N": 0, "O": 0},
+             "H2O2": {"H": 2, "C": 0, "N": 0, "O": 2},}
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
+from inspect import isdatadescriptor
 import numpy as np
 import math
 import cv2
 import sys
 import config
+import itertools
+from sklearn.cluster import DBSCAN
 
 def angle_to_rot(ax, ay, az):
     sx, sy, sz = np.sin(ax), np.sin(ay), np.sin(az)
@@ -13,34 +16,142 @@ def angle_to_rot(ax, ay, az):
     # Apply X rotation first, then Y, then Z
     return Rz @ Ry @ Rx
 
+def euc_distance(tvec1, tvec2):
+    return ((tvec1[0] - tvec2[0])**2 + (tvec1[1] - tvec2[1])**2 + (tvec1[2] - tvec2[2])**2)**0.5
+
+def calc_center(points, rvecs, tvecs):
+
+    t = np.zeros((len(points), 1, 3))
+    r = np.zeros((len(points), 1, 3))
+    i = 0
+    for p in points:
+        t[i] = tvecs[p][0]
+        r[i] = rvecs[p][0]
+        i += 1
+
+    rvec = np.mean(r, axis=0)
+    tvec = np.mean(t, axis=0)
+
+    return rvec, tvec
+
+def calc_position(rvec_m_c, tm_c):
+    points = np.array([[0, 0, 0, 1]]).T
+
+    R_m_p = np.array([[1, 0, 0],
+                    [0, 1, 0],
+                    [0, 0, 1]])
+
+    t_m_p = np.array([[0, 0, -config.height]]).T
+
+    R_m_c = cv2.Rodrigues(rvec_m_c)[0]
+
+    H_M_C = np.block([[R_m_c, tm_c.T], [0,0,0,1]])
+
+    H_M_P= np.block([[R_m_p, t_m_p], [0,0,0,1]])
+
+    H_P_M = np.linalg.inv(H_M_P)
+
+    H_P_C =  H_M_C @ H_P_M
+
+    Mext = H_P_C[0:3, :]
+
+    p = config.K @ Mext @ points
+    p = p / p[2]
+
+    p = p.T.astype(int)
+    return p
+
+def draw_element(img, id, rvec_m_c, tm_c):
+    if config.DEBUG:
+        img = cv2.aruco.drawAxis(
+            image=img, cameraMatrix=config.K, distCoeffs=config.dist_coeff,
+            rvec=rvec_m_c, tvec=tm_c, length=config.marker_length
+        )
+
+    p = calc_position(rvec_m_c, tm_c)
+
+    # print(f"x: {p[0][0]}, y: {p[0][1]}")
+
+    # Do something with mext to project image
+    
+    if (id == config.H_ID):
+        cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 0, 255), -1)
+        cv2.putText(img, "H", (p[0][0] - config.text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+    elif (id == config.C_ID):
+        cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 0), -1)
+        cv2.putText(img, "C", (p[0][0] - config.text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+    elif (id == config.N_ID):
+        cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 255), -1)
+        cv2.putText(img, "N", (p[0][0] - config.text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+    elif (id == config.O_ID):
+        cv2.circle(img, (p[0][0], p[0][1]), config.radius, (255, 0, 0), -1)
+        cv2.putText(img, "O", (p[0][0] - config.text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+
+def draw_molecule(img, molecule, points, rvecs, tvecs):
+    rvec, tvec = calc_center(points, rvecs, tvecs)
+
+    p = calc_position(rvec, tvec)
+
+    if (molecule == "H2O"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (127, 0, 255), -1)
+        cv2.putText(img, "H2O", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Water", (p[0][0] - 80, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "CO2"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (255, 127, 0), -1)
+        cv2.putText(img, "CO2", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Carbon-Dioxide", (p[0][0] - 150, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "N2"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (0, 255, 255), -1)
+        cv2.putText(img, "N2", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Nitrogen Gas", (p[0][0] - 150, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "O3"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (255, 0, 0), -1)
+        cv2.putText(img, "O3", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Ozone", (p[0][0] - 80, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "O2"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (255, 0, 0), -1)
+        cv2.putText(img, "O2", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Oxygen", (p[0][0] - 80, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "NH4"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (0, 64, 255), -1)
+        cv2.putText(img, "NH4", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Ammonium", (p[0][0] - 100, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "NH3"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (0, 96, 255), -1)
+        cv2.putText(img, "NH3", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Ammonia", (p[0][0] - 100, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "CH4"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (0, 64, 255), -1)
+        cv2.putText(img, "CH4", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Methane", (p[0][0] - 100, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+    elif (molecule == "H2O2"):
+        cv2.circle(img, (p[0][0], p[0][1]), config.mol_radius, (127, 0, 127), -1)
+        cv2.putText(img, "H2O2", (p[0][0] - config.mol_text_offset, p[0][1] + config.text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
+        cv2.putText(img, "Hydrogen Peroxide", (p[0][0] - 180, p[0][1] - config.mol_name_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(255, 255, 255), thickness=2)
+
+
 def main():
     # Read images from a video file in the current folder.
-    video_capture = cv2.VideoCapture(0)     # Open video capture object
+    video_capture = cv2.VideoCapture(1)     # Open video capture object
     got_image, img = video_capture.read()       # Make sure we can read video
     if not got_image:
         print("Cannot read video source")
         sys.exit()
 
-    image_height, image_width = img.shape[:2]
-    
-    dist_coeff = np.zeros((1,4))
+    if config.record:
+        image_height, image_width = img.shape[:2]
+        fourcc = cv2.VideoWriter_fourcc('m', 'p', '4', 'v')
+        videoWriter = cv2.VideoWriter("output.mp4", fourcc=fourcc, fps=30.0, frameSize=(image_width, image_height))
 
-    K = np.array([
-        [config.f, 0, config.x_c],
-        [0, config.f, config.y_c],
-        [0, 0, 1]]).astype(float)
-
-    points = np.array([[0, 0, 0, 1]]).T
-
-    arucoDict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_ARUCO_ORIGINAL)
 
     while True:
         got_image, img = video_capture.read()
 
         corners, ids, _ = cv2.aruco.detectMarkers(
             image=img,
-            dictionary=arucoDict
+            dictionary=config.arucoDict
         )
+
         if ids is not None:
             if config.DEBUG:
                 img = cv2.aruco.drawDetectedMarkers(
@@ -48,66 +159,60 @@ def main():
 
             rvecs,tvecs,_ = cv2.aruco.estimatePoseSingleMarkers(
                 corners=corners, markerLength=config.marker_length,
-                cameraMatrix=K, distCoeffs=dist_coeff
+                cameraMatrix=config.K, distCoeffs=config.dist_coeff
             )
 
+            # Remove un-recognized ids:
+            end = len(ids)
             i = 0
-            for id in ids:
-                # Get the pose of the detected marker with respect to the camera.
-                rvec_m_c = rvecs[i]                 # This is a 1x3 rotation vector
-                tm_c = tvecs[i]                     # This is a 1x3 translation vector
+            while i < end:
+                if ids[i][0] not in config.aruco_id_map:
+                    if config.DEBUG:
+                        print(f"Warning: unrecognized id, {ids[i]}")
+                    ids = np.delete(ids, i, 0)
+                    rvecs = np.delete(rvecs, i, 0)
+                    tvecs = np.delete(tvecs, i, 0)
+                    end -= 1
+                else:
+                    i += 1
 
-                if config.DEBUG:
-                    img = cv2.aruco.drawAxis(
-                        image=img, cameraMatrix=K, distCoeffs=dist_coeff,
-                        rvec=rvec_m_c, tvec=tm_c, length=config.marker_length
-                    )
+            if ids.size > 0:
+                squeezed = np.squeeze(tvecs, axis=1)
 
-                R_m_p = np.array([[1, 0, 0],
-                                [0, 1, 0],
-                                [0, 0, 1]])
+                clusters = DBSCAN(eps=config.dist_threshold, min_samples=1).fit(squeezed)
+                cluster_labels = clusters.labels_
+                num_clusters = max(clusters.labels_) + 1
 
-                t_m_p = np.array([[0, 0, -config.height]]).T
+                # Build the list of molecules
+                molecules = {}
+                
+                i = 0
+                for cluster_id in cluster_labels:
+                    if cluster_id not in molecules:
+                        molecules[cluster_id] = []
+                    molecules[cluster_id].append(i)
+                    i += 1
 
-                R_m_c = cv2.Rodrigues(rvec_m_c)[0]
+                for mol in molecules:
+                    element_sum = {"H": 0, "C": 0, "N": 0, "O": 0}
+                    for element in molecules[mol]:
+                        element_sum[config.aruco_id_map[ids[element][0]]] += 1
+                    is_molecule = False
+                    for m in config.molecules:
+                        if config.molecules[m] == element_sum:
+                            if config.DEBUG:
+                                print(m)
+                            
+                            draw_molecule(img, m, molecules[mol], rvecs, tvecs)
+                            is_molecule = True
+                    if not is_molecule:
+                        for element in molecules[mol]:
+                            draw_element(img, ids[element], rvecs[element], tvecs[element])
 
-                H_M_C = np.block([[R_m_c, tm_c.T], [0,0,0,1]])
-
-                H_M_P= np.block([[R_m_p, t_m_p], [0,0,0,1]])
-
-                H_P_M = np.linalg.inv(H_M_P)
-
-                H_P_C =  H_M_C @ H_P_M
-
-                Mext = H_P_C[0:3, :]
-
-                p = K @ Mext @ points
-                p = p / p[2]
-
-                p = p.T.astype(int)
-
-                print(f"x: {p[0][0]}, y: {p[0][1]}")
-
-                # Do something with mext to project image
-                text_offset = 15
-                if (id == config.H_ID):
-                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 0, 255), -1)
-                    cv2.putText(img, "H", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
-                elif (id == config.C_ID):
-                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 0), -1)
-                    cv2.putText(img, "C", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
-                elif (id == config.N_ID):
-                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (0, 255, 255), -1)
-                    cv2.putText(img, "N", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
-                elif (id == config.O_ID):
-                    cv2.circle(img, (p[0][0], p[0][1]), config.radius, (255, 0, 0), -1)
-                    cv2.putText(img, "O", (p[0][0] - text_offset, p[0][1] + text_offset), cv2.FONT_HERSHEY_PLAIN, 3, color=(0,0,0), thickness=2)
-
-                i += 1
-
+        if config.record:
+            img_output = img.copy()
+            videoWriter.write(img_output)
         cv2.imshow('Window', img)
-
-
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
